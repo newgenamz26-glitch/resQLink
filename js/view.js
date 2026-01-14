57a62115f4d6d8b9ad84c2f7d5adee4c512a3f62
newgenamz26-glitch <newgenamz26@gmail.com>
2026-01-14 11:44:03 +0800

    Add viewCaseDetail and closeDetail functions

---
+++ js/view.js
@@ -0,0 +1,21 @@
+window.viewCaseDetail = id => {
+  const c = allCases.find(x => x.id === id);
+  if (!c) return;
+
+  document.getElementById('detail-title').innerText = `ID KES: ${c.id}`;
+
+  document.getElementById('detail-body').innerHTML = `
+    <p><b>Responder:</b> ${c.program.responderName}</p>
+    <p><b>Program:</b> ${c.program.programName}</p>
+    <p><b>Pesakit:</b> ${c.patient.name}</p>
+    <p><b>Aduan:</b> ${c.medical.mainComplaint}</p>
+    <p><b>BP:</b> ${c.vitals.bp}</p>
+    <p><b>Status:</b> ${c.finalStatus}</p>
+  `;
+
+  document.getElementById('detail-modal').classList.add('show');
+};
+
+window.closeDetail = () => {
+  document.getElementById('detail-modal').classList.remove('show');
+};
